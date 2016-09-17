983803e1d28d30be592adea9500c52fc24eb5cac
hxim <hxim.jo@gmail.com>
2016-09-17 20:25:34 +0200

    pawnAttackWeight=80
    6181421

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -207,7 +207,7 @@ namespace {
   #undef V
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
-  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 40, 78, 56, 45, 11 };
+  const int KingAttackWeights[PIECE_TYPE_NB] = { 0, 80, 78, 56, 45, 11 };
 
   // Penalties for enemy's safe checks
   const int QueenContactCheck = 997;
